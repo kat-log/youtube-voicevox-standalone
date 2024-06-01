c75b7611158339c80a76fdf707945f49f79f8b3f
kat-log <greentea.tomatosystem@gmail.com>
2024-06-01 10:57:14 +0900

    add: APIキーが未入力の場合にメッセージを表示するように

--- package/background.js
+++ package/background.js
@@ -1,4 +1,10 @@
+// background.js
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (!request.apiKey) {
+    sendResponse({ status: "error", message: "APIキーが入力されていません。" });
+    return true; // Will respond asynchronously.
+  }
+
   let text = encodeURIComponent("こんにちは"); // 日本語の文字列をエンコード
   let url = `https://deprecatedapis.tts.quest/v2/voicevox/audio/?key=${request.apiKey}&speaker=0&pitch=0&intonationScale=1&speed=1&text=${text}`;
   sendResponse({ status: "success", audioUrl: url });
--- package/popup.js
+++ package/popup.js
@@ -4,8 +4,11 @@ document.getElementById("play").addEventListener("click", () => {
     if (response.status === "success") {
       let audio = new Audio(response.audioUrl);
       audio.play();
+      document.getElementById("error").textContent =
+        "APIキーの入力を確認しました"; // エラーメッセージなしの場合
     } else {
-      console.error("Error: ", response.message);
+      document.getElementById("error").textContent =
+        "Error: " + response.message; // エラーメッセージを表示
     }
   });
 });
