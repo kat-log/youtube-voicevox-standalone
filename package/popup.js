55ce4ee2b9521aa515866f1e55dec2d0a4ad94a4
kat-log <greentea.tomatosystem@gmail.com>
2024-06-02 07:14:06 +0900

    add: 最新のコメント１件のみ取得するように

--- package/popup.js
+++ package/popup.js
@@ -60,13 +60,15 @@ document.getElementById("play").addEventListener("click", () => {
       })
       .then((data) => {
         console.log("data : ", data);
-        let messages = data.items.map((item) => item.snippet.displayMessage);
+        // 最新のメッセージのみを取得
+        let latestMessage =
+          data.items[data.items.length - 1].snippet.displayMessage;
         document.getElementById("debug").textContent =
-          "Messages: " + messages.join(", ");
+          "Latest message: " + latestMessage;
 
         // メッセージにタイトルとメッセージを含めて送信
         chrome.runtime.sendMessage(
-          { apiKeyVOICEVOX: apiKeyVOICEVOX, messages: messages },
+          { apiKeyVOICEVOX: apiKeyVOICEVOX, messages: [latestMessage] },
           function (response) {
             if (response.status === "success") {
               let audio = new Audio(response.audioUrl);
