998e35dbf15474458af8244449ff67ea1b8c5b24
kat-log <greentea.tomatosystem@gmail.com>
2024-06-05 08:44:20 +0900

    refactor: ライブチャットIDを変数化してコメント取得の度に取得しなくて済むように、コードのモジュール化

--- package/background.js
+++ package/background.js
@@ -1,5 +1,6 @@
 let audio = null;
 let intervalId = null;
+let liveChatId = null;
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.action === "start") {
@@ -47,118 +48,129 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
         return;
       }
 
-      fetch(
-        `https://www.googleapis.com/youtube/v3/videos?id=${videoId}&part=liveStreamingDetails&key=${apiKeyYoutube}`
-      )
-        .then((response) => response.json())
-        .then((data) => {
-          if (
-            !data.items ||
-            data.items.length === 0 ||
-            !data.items[0].liveStreamingDetails
-          ) {
-            sendResponse({
-              status: "error",
-              message: "ライブストリーミングの詳細が見つかりません。",
-            });
-            return;
-          }
-
-          const liveChatId =
-            data.items[0].liveStreamingDetails.activeLiveChatId;
-          if (!liveChatId) {
-            sendResponse({
-              status: "error",
-              message:
-                "ライブチャットIDが見つかりません。（or APIのクエリ上限に達しているかも）",
-            });
-            return;
-          }
-
-          const requestUrl = `https://www.googleapis.com/youtube/v3/liveChat/messages?liveChatId=${liveChatId}&part=snippet,authorDetails&key=${apiKeyYoutube}`;
-          let latestMessage = "";
-
-          const checkNewComments = () => {
-            fetch(requestUrl)
-              .then((response) => {
-                if (!response.ok) {
-                  throw new Error("YouTube APIリクエストに失敗しました。");
-                }
-                return response.json();
-              })
-              .then((data) => {
-                if (!data.items || data.items.length === 0) {
-                  throw new Error("ライブチャットメッセージが見つかりません。");
-                }
-
-                let newMessage =
-                  data.items[data.items.length - 1].snippet.displayMessage;
-
-                if (newMessage !== latestMessage) {
-                  latestMessage = newMessage;
-
-                  fetchVoiceVox(apiKeyVOICEVOX, newMessage)
-                    .then((audioUrl) => {
-                      chrome.scripting.executeScript(
-                        {
-                          target: { tabId: tabs[0].id },
-                          func: (audioUrl) => {
-                            if (window.currentAudio) {
-                              window.currentAudio.pause();
-                            }
-                            let audio = new Audio(audioUrl);
-                            window.currentAudio = audio;
-                            audio.play();
-                          },
-                          args: [audioUrl],
-                        },
-                        () => {
-                          if (chrome.runtime.lastError) {
-                            console.error(
-                              "VoiceVoxエラー:",
-                              chrome.runtime.lastError.message
-                            );
-                          }
-                        }
-                      );
-                    })
-                    .catch((error) => {
-                      console.error("VoiceVoxエラー:", error);
-                    });
-                }
-
-                intervalId = setTimeout(checkNewComments, 2000);
-              })
-              .catch((error) => {
-                console.error("エラーをキャッチ:", error);
+      if (!liveChatId) {
+        fetch(
+          `https://www.googleapis.com/youtube/v3/videos?id=${videoId}&part=liveStreamingDetails&key=${apiKeyYoutube}`
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            if (
+              !data.items ||
+              data.items.length === 0 ||
+              !data.items[0].liveStreamingDetails
+            ) {
+              sendResponse({
+                status: "error",
+                message: "ライブストリーミングの詳細が見つかりません。",
               });
-          };
+              return;
+            }
 
-          checkNewComments();
-          sendResponse({ status: "success" });
-        })
-        .catch((error) => {
-          console.error("エラーをキャッチ:", error);
-          sendResponse({ status: "error", message: error.message });
-        });
+            liveChatId = data.items[0].liveStreamingDetails.activeLiveChatId;
+            if (!liveChatId) {
+              sendResponse({
+                status: "error",
+                message: "ライブチャットIDが見つかりません。",
+              });
+              return;
+            }
+
+            startFetchingComments(apiKeyVOICEVOX, apiKeyYoutube, tabs[0].id);
+            sendResponse({ status: "success" });
+          })
+          .catch((error) => {
+            console.error("エラーをキャッチ:", error);
+            sendResponse({ status: "error", message: error.message });
+          });
+      } else {
+        startFetchingComments(apiKeyVOICEVOX, apiKeyYoutube, tabs[0].id);
+        sendResponse({ status: "success" });
+      }
     });
 
     return true; // Will respond asynchronously.
   } else if (request.action === "stop") {
-    if (audio) {
-      audio.pause();
-      audio = null;
-    }
-    if (intervalId) {
-      clearTimeout(intervalId);
-      intervalId = null;
-    }
+    stopFetchingComments();
     sendResponse({ status: "success" });
     return true;
   }
 
   return true;
 });
+
+function startFetchingComments(apiKeyVOICEVOX, apiKeyYoutube, tabId) {
+  const requestUrl = `https://www.googleapis.com/youtube/v3/liveChat/messages?liveChatId=${liveChatId}&part=snippet,authorDetails&key=${apiKeyYoutube}`;
+  let latestMessage = "";
+
+  const checkNewComments = () => {
+    fetch(requestUrl)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("YouTube APIリクエストに失敗しました。");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        if (!data.items || data.items.length === 0) {
+          throw new Error("ライブチャットメッセージが見つかりません。");
+        }
+
+        let newMessage =
+          data.items[data.items.length - 1].snippet.displayMessage;
+
+        if (newMessage !== latestMessage) {
+          latestMessage = newMessage;
+
+          fetchVoiceVox(apiKeyVOICEVOX, newMessage)
+            .then((audioUrl) => {
+              chrome.scripting.executeScript(
+                {
+                  target: { tabId: tabId },
+                  func: (audioUrl) => {
+                    if (window.currentAudio) {
+                      window.currentAudio.pause();
+                    }
+                    let audio = new Audio(audioUrl);
+                    window.currentAudio = audio;
+                    audio.play();
+                  },
+                  args: [audioUrl],
+                },
+                () => {
+                  if (chrome.runtime.lastError) {
+                    console.error(
+                      "VoiceVoxエラー:",
+                      chrome.runtime.lastError.message
+                    );
+                  }
+                }
+              );
+            })
+            .catch((error) => {
+              console.error("VoiceVoxエラー:", error);
+            });
+        }
+
+        intervalId = setTimeout(checkNewComments, 2000);
+      })
+      .catch((error) => {
+        console.error("エラーをキャッチ:", error);
+      });
+  };
+
+  checkNewComments();
+}
+
+function stopFetchingComments() {
+  if (audio) {
+    audio.pause();
+    audio = null;
+  }
+  if (intervalId) {
+    clearTimeout(intervalId);
+    intervalId = null;
+  }
+}
 
 function fetchVoiceVox(apiKey, text) {
   const encodedText = encodeURIComponent(text);
